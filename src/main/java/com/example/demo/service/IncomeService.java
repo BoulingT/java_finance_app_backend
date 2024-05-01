bbd4cc8fbf4217aab30d2e0d64174e35705a51fa
Theo <119578352+BoulinguezTheo@users.noreply.github.com>
2024-05-02 00:08:09 +0200

    reafctoring income mapper

--- src/main/java/com/example/demo/service/IncomeService.java
+++ src/main/java/com/example/demo/service/IncomeService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.dto.incomes.MonthlyIncomeDto;
 import com.example.demo.entity.incomes.FixedIncome;
 import com.example.demo.entity.incomes.MonthlyIncomeList;
+import com.example.demo.mapper.MonthlyIncomeMapper;
 import com.example.demo.repository.incomes.MonthlyIncomeListRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
@@ -17,39 +18,16 @@ public class IncomeService {
 
     private final MonthlyIncomeListRepository monthlyIncomeListRepository;
 
+    private final MonthlyIncomeMapper monthlyIncomeMapper;
+
     public MonthlyIncomeDto getMonthlyIncomeList() throws BadRequestException {
         Optional<MonthlyIncomeList> monthlyIncomeOptional = monthlyIncomeListRepository.findById(1L);
         if (monthlyIncomeOptional.isPresent()) {
             MonthlyIncomeList monthlyIncome = monthlyIncomeOptional.get();
-            return mapMonthlyIncomeDto(monthlyIncome);
+            return monthlyIncomeMapper.mapMonthlyIncomeDto(monthlyIncome);
         } else {
             throw new BadRequestException("No monthly income list found");
         }
-    }
-
-    private MonthlyIncomeDto mapMonthlyIncomeDto(MonthlyIncomeList monthlyIncome) {
-        MonthlyIncomeDto monthlyIncomeDto = new MonthlyIncomeDto();
-        monthlyIncomeDto.setId(monthlyIncome.getId());
-        monthlyIncomeDto.setYearMonth(monthlyIncome.getActiveMonth());
-        monthlyIncomeDto.setSalary(getDesiredFixedIncome("salary", monthlyIncome.getFixedIncomeList()));
-        monthlyIncomeDto.setFreelance(getDesiredFixedIncome("freelance", monthlyIncome.getFixedIncomeList()));
-        monthlyIncomeDto.setAids(getAidsList(monthlyIncome.getFixedIncomeList()));
-        monthlyIncomeDto.setTotalIncomeAmount();
-
-        return monthlyIncomeDto;
-    }
-
-    private Set<FixedIncome> getAidsList(Set<FixedIncome> fixedIncomeList) {
-        return fixedIncomeList.stream()
-                .filter(fixedIncome -> fixedIncome.getIncomeType().getLabel().equals("aids"))
-                .collect(Collectors.toSet());
-    }
-
-    private FixedIncome getDesiredFixedIncome(String incomeTypeLabel, Set<FixedIncome> fixedIncomeList) {
-        return fixedIncomeList.stream()
-                .filter(fixedIncome -> fixedIncome.getIncomeType().getLabel().equals(incomeTypeLabel))
-                .findFirst()
-                .orElse(null);
     }
 }
 
